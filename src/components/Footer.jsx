b1c725911306431c34de02da44608479856e5a6a
SriD647 <sridatt.rao@hotmail.com>
2023-09-21 17:12:20 -0400

    udpated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="w-full my-8 max-w-7xl bg-[#1a1a29]">
       <div className="text-white opacity-60 grid grid-cols-1 sm:grid-cols-4 gap-10 justify-center items-center my-5 pt-5 pb-5">
-        <p className="text-center">@ Copyright 2023 | Sri Rao</p>
-        <p className="hidden sm:block text-center">Fullstack Engineer</p>
+        <p className="text-center">Copyright © 2023 | Sri Rao</p>
+        <p className="hidden sm:block text-center">Full-Stack Engineer</p>
         <p className="hidden sm:block text-center">Humanitarian</p>
         <p className="hidden sm:block text-center">Fitness Enthusiast</p>
       </div>
